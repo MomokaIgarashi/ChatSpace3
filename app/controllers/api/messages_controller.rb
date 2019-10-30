701c48cc97b64829515a2e812956e4cade4e2c1f
MomokaIgarashi <migarashi0318@gmail.com>
2019-10-30 16:42:10 +0900

    自動更新実装

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,12 @@
+class Api::MessagesController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user).where('id > ?', params[:id])
+
+    responde_to do |format|
+        format.html
+        format.json
+    end
+  end
+end
+
